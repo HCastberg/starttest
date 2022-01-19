4c8b6c57a3285caeaf8ae1366d0fab8b3df8fc4c
HCastberg <hermancastberg@gmail.com>
2022-01-19 14:12:39 +0100

    Cleaning

--- Variables.py
+++ Variables.py
@@ -1,14 +1,10 @@
-#Variable = container for value. Behaves as the value it contains.
-
+#Variables = container for value. Behaves as the value it contains.
 first_name = 'Herman'
 last_name = ' Castberg'
 full_name = first_name + last_name
 
-
-
 print('Hello, my name is:', full_name,'.')
 print(type(full_name))
-
 print( )
 
 #integer
@@ -18,14 +14,12 @@ print('My age is:', (age), 'years old')
 print(type(age), '25 is an integer')
 print( )
 
-
 #float = a decimal number
-
 height = 181.1
 print(height), print(type(height))
-
 print('Your height is: '+str(height),'cm')
 
 #Boolean = True or False
-human = False
+human = True
 print(human), print(type(human))
+print('Im a human: '+str(human))
