cdec2cf1f9ab5492996d38c8fc555d1df0f1db49
HCastberg <hermancastberg@gmail.com>
2022-01-19 14:06:02 +0100

    Variables
    Testing variables

---
+++ Variables.py
@@ -0,0 +1,31 @@
+#Variable = container for value. Behaves as the value it contains.
+
+first_name = 'Herman'
+last_name = ' Castberg'
+full_name = first_name + last_name
+
+
+
+print('Hello, my name is:', full_name,'.')
+print(type(full_name))
+
+print( )
+
+#integer
+age = 24
+age += 1
+print('My age is:', (age), 'years old')
+print(type(age), '25 is an integer')
+print( )
+
+
+#float = a decimal number
+
+height = 181.1
+print(height), print(type(height))
+
+print('Your height is: '+str(height),'cm')
+
+#Boolean = True or False
+human = False
+print(human), print(type(human))
